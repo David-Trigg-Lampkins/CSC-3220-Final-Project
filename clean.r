6be2281b6e3b2bdcfa8b143affd04b176517a89e
Trey Owen <treyeowen@gmail.com>
2025-11-06 14:28:03 -0600

    Convert to factors

--- clean.r
+++ clean.r
@@ -1,7 +1,17 @@
-library(tidyverse)
+library(dplyr)
 
 df <- read.csv("raw_data.csv")
 
-df
+# From Gemini
+df <- df |> mutate(across(c(RELATIONTO, TDOTLOC, TYPEOFCRAS, FIRSTHARMF, MANNEROFCO, WEATHER, LIGHTCONDI), 
+                          as.factor))
+
+df$DATEOFCRAS <- as.Date(df$DATEOFCRAS, format = "%m/%d/%Y")
+
+# Fix the mangled RELATIONTO column using regular expressions
+# DRIVEWAY, ALLEY AC -> DRIVEWAY, ALLEY ACCESS, ETC.
+# INTERSECTION RELA* -> INTERSECTION RELATED
+
+
 
 summary(df)
