8a55da8d90602048363e3aefae301f2f1ed8dd64
Trey Owen <treyeowen@gmail.com>
2025-11-06 14:45:47 -0600

    Remove unneeded columns

--- clean.r
+++ clean.r
@@ -9,9 +9,16 @@ df <- df |> mutate(across(c(RELATIONTO, TDOTLOC, TYPEOFCRAS, FIRSTHARMF, MANNERO
 df$DATEOFCRAS <- as.Date(df$DATEOFCRAS, format = "%m/%d/%Y")
 
 # Fix the mangled RELATIONTO column using regular expressions
-# DRIVEWAY, ALLEY AC -> DRIVEWAY, ALLEY ACCESS, ETC.
+# DRIVEWAY, ALLEY AC* -> DRIVEWAY, ALLEY ACCESS, ETC.
 # INTERSECTION RELA* -> INTERSECTION RELATED
 
+levels(df$RELATIONTO) <- gsub(df$RELATIONTO, "DRIVEWAY, ALLEY AC\\w+", "DRIVEWAY, ALLEY ACCESS, ETC.")
+levels(df$RELATIONTO) <- gsub(df$RELATIONTO, "INTERSECTION RELA\\w+", "INTERSECTION RELATED")
 
+# Drop unneeded columns (same value for all instances/useless data)
+df <- df |> select(-Shape.., 
+                   -MSLINK, -RELATIONT2, -URBAN, -NBR_TENN_C, 
+                   -NBR_RT2, -SPCL_CSE, -CNTY_SEQ, -YEAROFCRAS, 
+                   -LOCATE_TYP, -Hour)
 
 summary(df)
