561c5c4652dfda5cdb3560da27e53cc40043bf7e
Trey Owen <treyeowen@gmail.com>
2025-11-06 20:38:08 -0600

    Replace missing values with NAs

--- clean.r
+++ clean.r
@@ -2,23 +2,27 @@ library(dplyr)
 
 df <- read.csv("raw_data.csv")
 
+# Fix the mangled RELATIONTO column using regular expressions
+# DRIVEWAY* -> DRIVEWAY, ALLEY ACCESS, ETC.
+# INTERSECTION RELA* -> INTERSECTION RELATED
+
+df$RELATIONTO <- gsub("DRIVEWAY(\\w+|\\W)*$", "DRIVEWAY, ALLEY ACCESS, ETC.", df$RELATIONTO)
+df$RELATIONTO <- gsub("INTERSECTION RELA(\\W|\\w)*", "INTERSECTION RELATED", df$RELATIONTO)
+
+# Replace -- with NAs
+df[df == "--"] <- NA
+
 # From Gemini
-df <- df |> mutate(across(c(RELATIONTO, TDOTLOC, TYPEOFCRAS, FIRSTHARMF, MANNEROFCO, WEATHER, LIGHTCONDI), 
+df <- df |> mutate(across(c(RELATIONTO, TDOTLOC, TYPEOFCRAS, FIRSTHARMF, MANNEROFCO, WEATHER, LIGHTCONDI),
                           as.factor))
 
 df$DATEOFCRAS <- as.Date(df$DATEOFCRAS, format = "%m/%d/%Y")
 
-# Fix the mangled RELATIONTO column using regular expressions
-# DRIVEWAY, ALLEY AC* -> DRIVEWAY, ALLEY ACCESS, ETC.
-# INTERSECTION RELA* -> INTERSECTION RELATED
-
-levels(df$RELATIONTO) <- gsub(df$RELATIONTO, "DRIVEWAY, ALLEY AC\\w+", "DRIVEWAY, ALLEY ACCESS, ETC.")
-levels(df$RELATIONTO) <- gsub(df$RELATIONTO, "INTERSECTION RELA\\w+", "INTERSECTION RELATED")
 
 # Drop unneeded columns (same value for all instances/useless data)
-df <- df |> select(-Shape.., 
-                   -MSLINK, -RELATIONT2, -URBAN, -NBR_TENN_C, 
-                   -NBR_RT2, -SPCL_CSE, -CNTY_SEQ, -YEAROFCRAS, 
+df <- df |> select(-Shape..,
+                   -MSLINK, -RELATIONT2, -URBAN, -NBR_TENN_C,
+                   -NBR_RT2, -SPCL_CSE, -CNTY_SEQ, -YEAROFCRAS,
                    -LOCATE_TYP, -Hour)
 
 summary(df)
